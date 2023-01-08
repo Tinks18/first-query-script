a499b1418834535678a52c26fdab17b4e8f520a4
Tinks18 <tinutani9@gmail.com>
2023-01-08 17:55:04 +0000

    Add files

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//     $("h2").css("text-decoration","underline");
+//     $("ul").css("border","solid 10px #ccc");
+// });
+
+$(document).ready(function() {
+    $("h2").addClass("underline");
+    $("ul").addClass("border");
+});
+
+// $(document).ready(function() {
+//     $("h2").removeClass("underline");
+//     $("ul").removeClass("border");
+// });
